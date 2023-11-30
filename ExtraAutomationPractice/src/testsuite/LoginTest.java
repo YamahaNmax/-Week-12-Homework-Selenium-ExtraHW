2c4b0b77294e002b45eb3312939ac457f048c019
YamahaNmax <m4mitzlearning@gmail.com>
2023-11-30 16:50:28 +0000

    push

--- ExtraAutomationPractice/src/testsuite/LoginTest.java
+++ ExtraAutomationPractice/src/testsuite/LoginTest.java
@@ -48,6 +48,34 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
+    public void loginUserWithIncorrectEmailAndPassword() {
+
+        String actualText1 = driver.findElement(By.xpath("//div[@class='header-middle']/div[1]/div[1]/div[2]/div/ul/li[1]/a")).getText();
+        String expectedText1 = "Home";
+        Assert.assertEquals(actualText1, expectedText1);
+
+        WebElement loginText = driver.findElement(By.xpath("//div[@class='header-middle']/div[1]/div[1]/div[2]/div/ul/li[4]/a"));
+        loginText.click();
+
+        String actualText2 = driver.findElement(By.xpath("//div[@class='login-form']/h2")).getText();
+        String expectedText2 = "Login to your account";
+        Assert.assertEquals(actualText2, expectedText2);
+
+        WebElement emailLoginField = driver.findElement(By.xpath("//div[@class='login-form']/form/input[2]"));
+        emailLoginField.sendKeys("m4mitzcodebuster1@gmail.com");
+
+        WebElement passwordLoginField = driver.findElement(By.xpath("//div[@class='login-form']/form/input[3]"));
+        passwordLoginField.sendKeys("Codebuster");
+
+        WebElement loginButton = driver.findElement(By.xpath("//div[@class='login-form']/form/button"));
+        loginButton.click();
+
+        String actualText3 = driver.findElement(By.xpath("//div[@class='login-form']/form/p")).getText();
+        String expectedText3 = "Your email or password is incorrect!";
+        Assert.assertEquals(actualText3, expectedText3);
+    }
+
+    @Test
     public void logoutUser() {
 
         String actualText1 = driver.findElement(By.xpath("//div[@class='header-middle']/div[1]/div[1]/div[2]/div/ul/li[1]/a")).getText();
@@ -74,7 +102,7 @@ public class LoginTest extends BaseTest {
         String expectedText3 = "Logged in as Miten";
         Assert.assertEquals(actualText3, expectedText3);
 
-        WebElement logoutText =driver.findElement(By.xpath("//ul[@class='nav navbar-nav']/li[4]/a"));
+        WebElement logoutText = driver.findElement(By.xpath("//ul[@class='nav navbar-nav']/li[4]/a"));
         logoutText.click();
 
         String actualText4 = driver.findElement(By.xpath("//div[@class='login-form']/h2")).getText();
@@ -82,6 +110,35 @@ public class LoginTest extends BaseTest {
         Assert.assertEquals(actualText4, expectedText4);
 
     }
+
+    @Test
+    public void registerUserWithExistingEmail() {
+
+        String actualText1 = driver.findElement(By.xpath("//div[@class='header-middle']/div[1]/div[1]/div[2]/div/ul/li[1]/a")).getText();
+        String expectedText1 = "Home";
+        Assert.assertEquals(actualText1, expectedText1);
+
+        WebElement loginText = driver.findElement(By.xpath("//div[@class='header-middle']/div[1]/div[1]/div[2]/div/ul/li[4]/a"));
+        loginText.click();
+
+        String actualText2 = driver.findElement(By.xpath("//div[@class='signup-form']/h2")).getText();
+        String expectedText2 = "New User Signup!";
+        Assert.assertEquals(actualText2, expectedText2);
+
+        WebElement nameField = driver.findElement(By.xpath("//div[@class='signup-form']/form/input[2]"));
+        nameField.sendKeys("Yamaha");
+
+        WebElement emailRegisterField = driver.findElement(By.xpath("//div[@class='signup-form']/form/input[3]"));
+        emailRegisterField.sendKeys("m4mitzcodebuster@gmail.com");
+
+        WebElement signUpButton = driver.findElement(By.xpath("//div[@class='signup-form']/form/button"));
+        signUpButton.click();
+
+        String actualText3 = driver.findElement(By.xpath("//div[@class='signup-form']/form/p")).getText();
+        String expectedText3 = "Email Address already exist!";
+        Assert.assertEquals(actualText3, expectedText3);
+    }
+
 
     @After
     public void endTest() {
